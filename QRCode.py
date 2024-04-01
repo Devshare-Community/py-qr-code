8c6f11c8cf7eeef2b915a6c06ef930e890d965fb
Exousia KASEKA <kasekaexousia@gmail.com>
2024-04-01 12:26:21 +0100

    Importation de différents library dont on aura besoin

---
+++ QRCode.py
@@ -0,0 +1,9 @@
+from tkinter import *
+from tkinter import messagebox
+from tkinter.filedialog import askopenfilename
+import os
+import shutil
+import barcode
+import MyQR
+import csv
+from datetime import datetime
