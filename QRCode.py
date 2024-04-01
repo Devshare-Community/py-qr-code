2c9d1f7214875563f6fddf9a9a5a407c1d5cb6bf
Exousia KASEKA <kasekaexousia@gmail.com>
2024-04-01 12:27:56 +0100

    Creating the base window for the GUI

--- QRCode.py
+++ QRCode.py
@@ -7,3 +7,11 @@ import barcode
 import MyQR
 import csv
 from datetime import datetime
+
+
+s = 0
+# Creating the base window for the GUI
+window = Tk()
+window.resizable(width=False, height=False)
+window.title("QR and Bar Code Generator")
+
