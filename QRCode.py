eac2b5f8c28896c90256cee7eebfe4a31b0760c1
Exousia KASEKA <kasekaexousia@gmail.com>
2024-04-02 11:10:21 +0100

    fucn to store data in csv files

--- QRCode.py
+++ QRCode.py
@@ -23,3 +23,24 @@ def setsize(a):
     messagebox.showinfo("", "Select format")
 
 
+def open_file():
+    Tk().withdraw()  # we don't want a full GUI, so keep the root window from appearing
+    file = askopenfilename()  # show an "Open" dialog box and return the path to the selected file
+    return file
+
+
+# fucn to store data in csv files
+def storedata():
+    if not os.path.isfile(os.getcwd() + "\\QrCode and Barcode Datas.csv"):
+        with open("QrCode and Barcode Datas.csv", "w", newline="") as codes:
+            fields = ["Subject", "Type", "Timestamp"]
+            writer = csv.DictWriter(codes, fieldnames=fields)
+            writer.writeheader()
+    with open("QrCode and Barcode Datas.csv", "a", newline="") as codes:
+        writer = csv.writer(codes)
+        if type1 == 1:
+            writer.writerow([Subject.get(), "Qrcode", timestampStr1])
+        elif type1 == 2:
+            writer.writerow([Subject.get(), "Barcode", timestampStr2])
+
+
